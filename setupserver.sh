da2ad512f4d3edad30ec80825640a07cd225d1f2
Naheed Roomy <nroomy1@gmail.com>
2023-10-07 22:16:01 +0000

    added setup script

---
+++ setupserver.sh
@@ -0,0 +1,38 @@
+!/bin/bash
+
+# Update the package list and install nginx
+sudo apt update
+sudo apt install -y nginx
+
+# Create the nginx server block configuration
+cat <<EOL | sudo tee /etc/nginx/sites-available/valorant.conf
+server {
+    listen 80;
+    server_name www.valorantsl.com valorantsl.com;
+
+    location / {
+        proxy_pass http://localhost:8501;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+}
+EOL
+
+# Create a symbolic link to enable the configuration
+sudo ln -s /etc/nginx/sites-available/valorant.conf /etc/nginx/sites-enabled/
+
+# Remove the default server block if it exists
+if [ -L /etc/nginx/sites-enabled/default ]; then
+    sudo rm /etc/nginx/sites-enabled/default
+fi
+
+# Test nginx configuration and reload nginx
+sudo nginx -t && sudo systemctl reload nginx
+
+# Output to check if nginx reloaded successfully
+if [ $? -eq 0 ]; then
+    echo "Nginx reloaded successfully!"
+else
+    echo "Nginx failed to reload. Please check the configuration."
+fi
