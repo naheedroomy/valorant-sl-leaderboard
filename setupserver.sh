7f9df04ab56761464ea6646060b443c74a8ce132
Naheed Roomy <46345736+naheedroomy@users.noreply.github.com>
2023-10-08 00:49:04 +0200

    Update setupserver.sh

--- setupserver.sh
+++ setupserver.sh
@@ -21,9 +21,15 @@ server {
 
     location / {
         proxy_pass http://localhost:8501;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+        # WebSocket support
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_read_timeout 86400;
     }
 }
 EOL
