c36ca9b2cd57da1218de0cd3fdb653887b3ee56f
Naheed Roomy <nroomy1@gmail.com>
2023-10-29 05:24:16 +0100

    fix: discord bots limited to 3 again

--- utils/discord_bot.py
+++ utils/discord_bot.py
@@ -32,17 +32,29 @@ db = mongo_client["live"]
 collection = db["user_leaderboard"]
 
 
+async def http_request(session, url, method='GET', **kwargs):
+    delay = 1
+    while True:
+        async with session.request(method, url, **kwargs) as response:
+            if response.status == 429:
+                # Rate limited
+                retry_after = float(response.headers.get('Retry-After', 1))
+                logging.warning(f"Rate limited. Retrying after {retry_after} seconds.")
+                await asyncio.sleep(retry_after)
+            else:
+                return response
+
 async def fetch_tier_data():
     async with aiohttp.ClientSession() as session:
-        async with session.get('https://valorant-api.com/v1/competitivetiers') as response:
-            if response.status == 200:
-                data = await response.json()
-                tiers = data['data'][0]['tiers']
-                tier_icons = {tier['tierName']: tier['smallIcon'] for tier in tiers if tier['smallIcon']}
-                return tier_icons
-            else:
-                logging.error(f"Failed to fetch tier data: {response.status}")
-                return {}
+        response = await http_request(session, 'https://valorant-api.com/v1/competitivetiers')
+        if response.status == 200:
+            data = await response.json()
+            tiers = data['data'][0]['tiers']
+            tier_icons = {tier['tierName']: tier['smallIcon'] for tier in tiers if tier['smallIcon']}
+            return tier_icons
+        else:
+            logging.error(f"Failed to fetch tier data: {response.status}")
+            return {}
 
 
 async def update_alpha_omega_roles(member, rank):
@@ -98,11 +110,31 @@ async def update_member_roles(member, tier_icons):
             collection.update_one(update_query, new_values)
 
         verified_role = discord.utils.get(member.guild.roles, name="Verified")
-        await member.add_roles(verified_role)
+        try:
+            await member.add_roles(verified_role)
+        except discord.errors.HTTPException as e:
+            if e.status == 429:
+                retry_after = e.response.headers.get('Retry-After')
+                if retry_after:
+                    logging.warning(f"Rate limited. Waiting for {retry_after} seconds.")
+                    await asyncio.sleep(int(retry_after))
+                    await member.add_roles(verified_role)
+            else:
+                logging.error(f"HTTP exception: {e}")
 
         unverified_role = discord.utils.get(member.guild.roles, name="Unverified")
         if unverified_role in member.roles:
-            await member.remove_roles(unverified_role)
+            try:
+                await member.remove_roles(unverified_role)
+            except discord.errors.HTTPException as e:
+                if e.status == 429:
+                    retry_after = e.response.headers.get('Retry-After')
+                    if retry_after:
+                        logging.warning(f"Rate limited. Waiting for {retry_after} seconds.")
+                        await asyncio.sleep(int(retry_after))
+                        await member.remove_roles(unverified_role)
+                else:
+                    logging.error(f"HTTP exception: {e}")
             logging.info(f"Removed unverified role from {discord_username}.")
 
         rank = result.get("rank")
@@ -121,6 +153,16 @@ async def update_member_roles(member, tier_icons):
                 logging.info(f"Updated nickname - {discord_username} - {new_nickname}.")
             except discord.errors.Forbidden:
                 logging.error(f"Failed to update nickname for {discord_username} due to insufficient permissions.")
+            except discord.errors.HTTPException as e:
+                if e.status == 429:
+                    retry_after = e.response.headers.get('Retry-After')
+                    if retry_after:
+                        logging.warning(f"Rate limited. Waiting for {retry_after} seconds.")
+                        await asyncio.sleep(int(retry_after))
+                        await member.edit(nick=new_nickname)
+                        logging.info(f"Updated nickname - {discord_username} - {new_nickname}.")
+                else:
+                    logging.error(f"HTTP exception: {e}")
 
         if rank:
             rank_roles = ['Ascendant', 'Diamond', 'Immortal', 'Radiant', 'Gold', 'Platinum', 'Silver', 'Iron', 'Bronze',
@@ -129,25 +171,67 @@ async def update_member_roles(member, tier_icons):
 
             if not current_rank_role or current_rank_role.name != rank:
                 if current_rank_role:
-                    await member.remove_roles(current_rank_role)
-                    logging.info(f"Removed - {discord_username} - {current_rank_role.name}.")
+                    try:
+                        await member.remove_roles(current_rank_role)
+                        logging.info(f"Removed - {discord_username} - {current_rank_role.name}.")
+                    except discord.errors.HTTPException as e:
+                        if e.status == 429:
+                            retry_after = e.response.headers.get('Retry-After')
+                            if retry_after:
+                                logging.warning(f"Rate limited. Waiting for {retry_after} seconds.")
+                                await asyncio.sleep(int(retry_after))
+                                await member.remove_roles(current_rank_role)
+                                logging.info(f"Removed - {discord_username} - {current_rank_role.name}.")
+                        else:
+                            logging.error(f"HTTP exception: {e}")
 
                 rank_role = discord.utils.get(member.guild.roles, name=rank)
                 if rank_role:
-                    await member.add_roles(rank_role)
-                    logging.info(f"Updated - {discord_username} - {rank}.")
+                    try:
+                        await member.add_roles(rank_role)
+                        logging.info(f"Updated - {discord_username} - {rank}.")
+                    except discord.errors.HTTPException as e:
+                        if e.status == 429:
+                            retry_after = e.response.headers.get('Retry-After')
+                            if retry_after:
+                                logging.warning(f"Rate limited. Waiting for {retry_after} seconds.")
+                                await asyncio.sleep(int(retry_after))
+                                await member.add_roles(rank_role)
+                                logging.info(f"Updated - {discord_username} - {rank}.")
+                        else:
+                            logging.error(f"HTTP exception: {e}")
                 else:
                     logging.error(f"Role not found for rank: {rank}")
 
             if rank == 'Unranked':
                 manual_role = discord.utils.get(member.guild.roles, name="Manual")
                 if manual_role not in member.roles:
-                    await member.add_roles(manual_role)
+                    try:
+                        await member.add_roles(manual_role)
+                    except discord.errors.HTTPException as e:
+                        if e.status == 429:
+                            retry_after = e.response.headers.get('Retry-After')
+                            if retry_after:
+                                logging.warning(f"Rate limited. Waiting for {retry_after} seconds.")
+                                await asyncio.sleep(int(retry_after))
+                                await member.add_roles(manual_role)
+                        else:
+                            logging.error(f"HTTP exception: {e}")
 
             await update_alpha_omega_roles(member, rank)
     else:
         unverified_role = discord.utils.get(member.guild.roles, name="Unverified")
-        await member.add_roles(unverified_role)
+        try:
+            await member.add_roles(unverified_role)
+        except discord.errors.HTTPException as e:
+            if e.status == 429:
+                retry_after = e.response.headers.get('Retry-After')
+                if retry_after:
+                    logging.warning(f"Rate limited. Waiting for {retry_after} seconds.")
+                    await asyncio.sleep(int(retry_after))
+                    await member.add_roles(unverified_role)
+            else:
+                logging.error(f"HTTP exception: {e}")
 
 
 @client.event
